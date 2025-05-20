b936a6fa3ab122a236289126203efd146f6952aa
Himani Mehra <himanimehra@Himanis-MacBook-Air.local>
2025-05-20 16:42:08 +0530

    used hash router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,69 @@
+// import Header from "./components/header/Header";
+// import Footer from "./components/footer/Footer";
+// import Error from "./components/Error";
+// import Home from "./components/home/Home";
+// import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+// import { taskRoutes } from "./constants";
+// import { TaskProvider } from "./utils/TaskContext";
+
+// const AppLayout = () => {
+//   return (
+//     <TaskProvider>
+//       <Header />
+//       <Outlet />
+//       <Footer />
+//     </TaskProvider>
+//   );
+// };
+
+// const appRouter = createBrowserRouter([
+//   {
+//     path: "/react-task-gallery",
+//     element: <AppLayout />,
+//     errorElement: <Error />,
+//     children: [
+//       {
+//         path: "/react-task-gallery",
+//         element: <Home />
+//       },
+//       ...taskRoutes
+//     ]
+//   }
+// ]);
+
+// function App() {
+//   return <RouterProvider router={appRouter} />;
+// }
+
+// export default App;
+
+
+
+import React from "react";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Error from "./components/Error";
 import Home from "./components/home/Home";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route, Outlet } from "react-router-dom";
 import { taskRoutes } from "./constants";
 import { TaskProvider } from "./utils/TaskContext";
 
-const AppLayout = () => {
-  return (
-    <TaskProvider>
-      <Header />
-      <Outlet />
-      <Footer />
-    </TaskProvider>
-  );
-};
-
-const appRouter = createBrowserRouter([
-  {
-    path: "/react-task-gallery",
-    element: <AppLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "/react-task-gallery",
-        element: <Home />
-      },
-      ...taskRoutes
-    ]
-  }
-]);
-
 function App() {
-  return <RouterProvider router={appRouter} />;
+  return (
+    <HashRouter>
+      <TaskProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {taskRoutes.map(({ path, element }, idx) => (
+            <Route key={idx} path={path} element={element} />
+          ))}
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </TaskProvider>
+    </HashRouter>
+  );
 }
 
 export default App;
